2e2311d9d7847ea2f159c58756387af2e76225e9
ingvord <ingvord.mail@gmail.com>
2024-11-16 01:37:25 +0100

    Progress

--- pulsar.sh
+++ pulsar.sh
@@ -5,5 +5,6 @@ docker run -it \
 -p 8080:8080 \
 --mount source=pulsardata,target=/pulsar/data \
 --mount source=pulsarconf,target=/pulsar/conf \
+--name pulsar-standalone \
 apachepulsar/pulsar:4.0.0 \
 bin/pulsar standalone
